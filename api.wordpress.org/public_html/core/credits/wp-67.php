7cb9d693437886cd4f1f8217d41bd066ffc16610
Dion Hulse <dd32@git.wordpress.org>
2024-11-25 03:08:50 +0000

    API: Credits: Add the LibFont library as an external libraries in the credits for 6.5+.
    Props mmaattiiaass, im3dabasia1, dd32.
    Fixes #7825.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@14197 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/core/credits/wp-67.php
+++ api.wordpress.org/public_html/core/credits/wp-67.php
@@ -950,6 +950,7 @@ class WP_67_Credits extends WP_Credits {
 			[ 'jQuery.suggest', 'https://github.com/pvulgaris/jquery.suggest' ],
 			[ 'jQuery UI Touch Punch', 'https://github.com/furf/jquery-ui-touch-punch' ],
 			[ 'json2', 'https://github.com/douglascrockford/JSON-js' ],
+			[ 'LibFont', 'https://github.com/Pomax/lib-font' ],
 			[ 'Lodash', 'https://lodash.com/' ],
 			[ 'Masonry', 'https://masonry.desandro.com/' ],
 			[ 'MediaElement.js', 'https://www.mediaelementjs.com/' ],
